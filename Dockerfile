743cf00d95199aac102d3f5e88c256860a2ee429
Bentley Hensel <bentley.hensel@civicactions.com>
2023-04-13 22:12:37 +0000

    Dockerfile updates & Proxy Setup

--- Dockerfile
+++ Dockerfile
@@ -58,8 +58,15 @@ COPY src /app/src
 # Install axe-cli
 RUN npm install -g @axe-core/cli
 
-# Make port 8083 available to the world outside this container
-EXPOSE 8083
+# Make port available to the world outside this container
+# The port number is now set as an environment variable with a default value of 8083
+ENV APP_PORT 8083
+
+# Set up the proxy environment variables
+ENV http_proxy http://192.168.1.15:18888
+ENV https_proxy http://192.168.1.15:18888
+
+EXPOSE $APP_PORT
 
 # Define environment variable
 ENV FLASK_APP axe.py
--- src/axe.py
+++ src/axe.py
@@ -1,3 +1,4 @@
+import os
 import json
 import subprocess
 from flask import Flask, request, jsonify
@@ -6,6 +7,7 @@ with open('src/axe.json') as f:
     header_mapping = json.load(f)
 
 app = Flask(__name__)
+
 
 def process_response(response_dict, mapping):
     processed = {}
@@ -29,16 +31,20 @@ def process_response(response_dict, mapping):
 
 @app.route('/axe')
 def axe_scan():
-    proxy_address = 'http://192.168.1.15:18888'
     url = request.args.get('url')
-    cmd = f'axe {url} --chromedriver-path /usr/local/bin/chromedriver --chrome-options="no-sandbox" --proxy-server={proxy_address} --stdout'
+    # Get the proxy from the environment variable
+    proxy = os.environ.get('http_proxy')
+    cmd = f'axe {url} --chromedriver-path /usr/local/bin/chromedriver --chrome-options="no-sandbox" --stdout'
+    if proxy:
+        # Prepend the proxy environment variables to the command
+        cmd = f'http_proxy={proxy} https_proxy={proxy} ' + cmd
     output = subprocess.check_output(cmd, shell=True)
     response = json.loads(output.decode('utf-8'))
 
     # Check if the response is a list and get the first item
     if isinstance(response, list) and len(response) > 0:
         response = response[0]
- # with open('../maps/axe.json') as f:
+    # with open('../maps/axe.json') as f:
     # Define header mapping
     with open('src/axe.json') as f:
         header_mapping = json.load(f)
@@ -51,4 +57,6 @@ def axe_scan():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8083)
+    # Get the port number from the environment variable or use 8083 as default
+    app_port = int(os.environ.get('APP_PORT', 8083))
+    app.run(debug=True, host='0.0.0.0', port=app_port)
